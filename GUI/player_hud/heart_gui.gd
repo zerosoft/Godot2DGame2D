63865bd01f3d8f7292b6b20d811f0ca064d804ef
Zero <zerosoft@vip.qq.com>
2025-05-11 21:46:53 +0800

    血条一半

---
+++ GUI/player_hud/heart_gui.gd
@@ -0,0 +1,3 @@
+class_name HeartGUI extends Control
+
+@onready var sprite_2d = $Sprite2D
---
+++ GUI/player_hud/player_hud.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+var hearts:Array[Control] = []
+
+func _ready() -> void:
+	pass
+
+func update_hp(health:int,_max_hp:int) -> void:
+	
+	pass
